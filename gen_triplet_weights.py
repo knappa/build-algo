a7b5b0936b15d0de8ceea5dbd6df3f2822c9fd57
Adam Knapp <adam.knapp@instanton.org>
2024-08-15 14:54:29 -0400

    move playground code out

--- gen_triplet_weights.py
+++ gen_triplet_weights.py
@@ -2,17 +2,27 @@
 # generate triplet weights
 
 import argparse
+import itertools
 from typing import Dict
 
 import numpy as np
-import scipy
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--phylip", required=True, help="phylip file")
 parser.add_argument("--output", required=True, help="output file")
 
-opt = parser.parse_args()
-print(opt)
+
+try:
+    opt = parser.parse_args()
+    print(opt)
+except SystemExit:
+    # options for pasting into ipython
+    class Object:
+        pass
+
+    opt = Object()
+    opt.phylip = "data/alignment_GTR_30_taxa_1K_sites_rtree1.phy"
+    opt.output = "test.txt"
 
 
 def seq_to_array(seq: str, seq_len_: int) -> np.ndarray:
@@ -30,8 +40,7 @@ def seq_to_array(seq: str, seq_len_: int) -> np.ndarray:
 
 
 data: Dict[str, np.ndarray] = dict()
-# with open(opt.phylip, 'r') as file:
-with open("data/alignment_GTR_30_taxa_1K_sites_rtree1.phy", "r") as file:
+with open(opt.phylip, "r") as file:
     n_species, seq_len = map(lambda s: int(s.strip()), file.readline().strip().split())
 
     for line in file:
@@ -40,109 +49,8 @@ with open("data/alignment_GTR_30_taxa_1K_sites_rtree1.phy", "r") as file:
         assert len(sequence) == seq_len, f"seq length mismatch for {species}"
         data[species] = seq_to_array(sequence, seq_len)
 
-import itertools
 
-
-# noinspection PyPep8Naming
-def cofactor(M):
-    # noinspection PyShadowingNames
-    N = np.zeros_like(M)
-    for r, c in itertools.product(range(M.shape[0]), range(M.shape[1])):
-        # noinspection PyTypeChecker
-        N[r, c] = np.linalg.det(
-            np.block(
-                [[M[0:r, 0:c], M[0:r, c + 1 :]], [M[r + 1 :, 0:c], M[r + 1 :, c + 1 :]]]
-            )
-        )
-    return N
-
-
-# noinspection PyPep8Naming
-def GTR(pis_, rcs_):
-    pi_a, pi_c, pi_g, pi_t = pis_
-    a, b, c, d, e, f = rcs_
-    Q_ = np.array(
-        [
-            [-(a * pi_c + b * pi_g + c * pi_t), a * pi_c, b * pi_g, c * pi_t],
-            [a * pi_c, -(a * pi_c + d * pi_g + e * pi_t), d * pi_g, e * pi_t],
-            [b * pi_a, d * pi_c, -(b * pi_a + d * pi_c + f * pi_t), f * pi_t],
-            [c * pi_a, e * pi_c, f * pi_g, -(c * pi_a + e * pi_c + f * pi_g)],
-        ]
-    )
-    return Q_
-
-
-pis = (0.3, 0.25, 0.2, 0.25)
-rcs = (1, 1, 1, 1, 1, 1)
-
-N = 1000
-
-Q = GTR(pis, rcs)
-
-
-def sample(pis_):
-    return int(np.argmin(np.cumsum(pis_) < np.random.rand()))
-
-
-# noinspection PyPep8Naming,PyShadowingNames
-def cond_sample(P, D):
-    return [sample(P[d, :]) for d in D]
-
-
-def tree(ai, bi, i, ic):
-    root_dist = [sample(pis) for _ in range(N)]
-    i_dist = cond_sample(scipy.linalg.expm(i * Q), root_dist)
-    a_dist = cond_sample(scipy.linalg.expm(ai * Q), i_dist)
-    b_dist = cond_sample(scipy.linalg.expm(bi * Q), i_dist)
-    c_dist = cond_sample(scipy.linalg.expm(ic * Q), root_dist)
-
-    return np.array(a_dist), np.array(b_dist), np.array(c_dist)
-
-
-a_seq, b_seq, c_seq = tree(1, 1, 1, 2)
-P_sim = np.zeros((4, 4, 4), dtype=np.float64)
-for site_idx in range(seq_len):
-    P_sim[a_seq[site_idx], b_seq[site_idx], c_seq[site_idx]] += 1
-
-P_sim_false1 = P_sim.transpose((0, 2, 1))
-P_sim_false2 = P_sim.transpose((1, 2, 0))
-
-for P in [P_sim, P_sim_false1, P_sim_false2]:
-    for k in range(4):
-        A = P[:, :, k]
-        B = np.sum(P, axis=1)
-        C = np.sum(P, axis=0)
-        D = -A.T
-        E = -B.T
-        F = -C.T
-        print(
-            np.linalg.svd(
-                C @ np.linalg.inv(B) @ A + D @ np.linalg.inv(E) @ F, compute_uv=False
-            )
-        )
-    print()
-
-for a_species, b_species, c_species in itertools.combinations(data.keys(), 3):
-    P_ab = np.zeros((4, 4, 4), dtype=np.float64)
-    a_seq, b_seq, c_seq = data[a_species], data[b_species], data[c_species]
-    for site_idx in range(seq_len):
-        P_ab[a_seq[site_idx], b_seq[site_idx], c_seq[site_idx]] += 1
-
-    for k in range(4):
-        A = P_ab[:, :, k]
-        B = np.sum(P_ab, axis=1)
-        C = np.sum(P_ab, axis=0)
-        D = -A.T
-        E = -B.T
-        F = -C.T
-        print(
-            np.linalg.svd(
-                C @ np.linalg.inv(B) @ A + D @ np.linalg.inv(E) @ F, compute_uv=False
-            )
-        )
-
-# with open(opt.output, 'w') as file:
-with open("test.txt", "w") as file:
+with open(opt.output, "w") as file:
     for a_species, b_species, c_species in itertools.combinations(data.keys(), 3):
         P_ab = np.zeros((4, 4, 4), dtype=np.float64)
         a_seq, b_seq, c_seq = data[a_species], data[b_species], data[c_species]
