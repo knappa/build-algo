f5ab13d4ea6e8bd016fbfe6113e4492c2e68e5bf
Adam Knapp <adam.knapp@instanton.org>
2025-07-24 14:10:28 -0400

    fixes for script version

--- src/build_algo/build.py
+++ src/build_algo/build.py
@@ -5,7 +5,7 @@
 def main_cli():
     import argparse
 
-    from build_algo import _gen_tree, get_triplets_from_file
+    from build_algo import gen_tree, get_triplets_from_file
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--triplets", required=True, help="triplets file")
@@ -15,4 +15,4 @@ def main_cli():
 
     all_species, all_triplets = get_triplets_from_file(opt.triplets)
 
-    print(_gen_tree(all_triplets))
+    print(gen_tree(all_triplets))
