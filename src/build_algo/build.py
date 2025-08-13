64006f4a41cb3ccf7ba95e477e7be8c140116b55
Adam Knapp <adam.knapp@instanton.org>
2025-08-13 09:39:55 -0400

    add viz argument for command line program

--- src/build_algo/build.py
+++ src/build_algo/build.py
@@ -10,9 +10,16 @@ def main_cli():
     parser = argparse.ArgumentParser()
     parser.add_argument("--triplets", required=True, help="triplets file")
 
+    parser.add_argument("--viz", action="store_true", help="print visualization")
+
     opt = parser.parse_args()
     # print(opt)
 
     all_species, all_triplets = get_triplets_from_file(opt.triplets)
 
-    print(gen_tree(all_triplets))
+    tree = gen_tree(all_triplets)
+
+    if opt.viz:
+        print(tree)
+    else:
+        print(tree.write(format=9))
